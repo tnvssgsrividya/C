7f366cf3b12abdc03826ea1f8c09641d05fd90a6
tnvssgsrividya <tnvssgsrividya@gmail.com>
2017-01-24 23:11:08 +0530

    Create square.c

---
+++ square.c
@@ -0,0 +1,23 @@
+//program to find square of a number 
+
+#include<stdio.h>
+main()              //main() is a function
+{
+   int num,square;   //variable declaration
+     
+      printf("Enter a number for which square is to be calculated : ");
+      scanf("%d",&num);
+      
+      suare=num*num;
+      
+      printf("\nThe square for %d is %d ",num,square);
+}      
+
+
+
+
+
+output:
+
+Enter a  number for which square is to be calculated : 15
+The suare for 15 is 225
